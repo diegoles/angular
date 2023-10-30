9a024e837654567646ee39e130c23aaee58d699b
Edgar Diaz <ediaz@ec.krugercorp.com>
2023-10-30 15:54:31 -0500

    Curso Angular 7

--- README.md
+++
@@ -1,2 +0,0 @@
-# angular
-Ejemplos con angular
---
+++ clientes-app/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title:string = 'Bienvenido al curso Angular 7 !!!!';
+
+  curso:string = 'Curso Spring 5 con angular 7';
+
+  profesor:string = 'Edgar Diaz';
+}
